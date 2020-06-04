d43a3838531919ee9bec65d305adb152e3618f1f
pointlesspun <anotherpointlesspun@gmail.com>
2020-06-04 11:03:41 +0200

    update server front end page

--- back-end-server.js
+++ back-end-server.js
@@ -66,7 +66,7 @@ exports.start = function() {
  * @param response 
  */
 exports.renderDefaultWebPage = function (response)  {
-	response.send('Laurette - Server, thesis pre-alpha, 26/5/2020 --'
+	response.send('Laurette - Warehouse Server, thesis ALPHA, 04/6/2020 --'
 				+ '<br> ' + Object.keys(_userCredentials).length + ' users logged in.');
 }
 
